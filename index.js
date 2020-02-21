eb7186694d36684762cd3288d2820622c66222ae
bobo023t <39660579+bobo023t@users.noreply.github.com>
2020-02-21 20:45:07 +0800

    new awa

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client();
-const token = process.env.token
+const token = process.env.token;
 const { CommandHandler } = require("djs-commands")
 const active = new Map();
 const prefix = require('./commands/prefixes.json')
 const ownerID = '483564053898199040';
+const ver = require('C:/Users/Bobo/Desktop/DC-master/verfify/mc.js')
+const fs = require('fs')
 const CH = new CommandHandler({
     folder: __dirname + '/commands/',
     prefix: [prefix.prefix]
   });
+var target = ('')
+var target1
 
 bot.on('ready', () => {
   	bot.user.setStatus('online')
@@ -38,15 +42,68 @@ bot.on("message", (message) => {
        ownerID: ownerID,
        active: active
      }
-     if(!cmd) return;
+     //if(!cmd) return;
 
       try{
           cmd.run(bot, message, args, ops)
       }catch(e){
-          console.log(e)
+          //console.log(e)
       }
-
+    if(args[0] == 'vertify'){
+	if(message.author.id != '494520333739753472' || message.author.id != '648457616384851990' )
+    message.channel.send('按下❤️驗證')
+      .then(function (message) 
+      {message.react('❤️')
+      target = message.id
+      fs.writeFile('target.json', target, function (err) {
+        if (err)
+            console.log(err);
+        else
+            console.log('Write operation complete.');
+    });
+    })
+    }
   });
-
+  bot.on('raw', packet => {
+  fs.readFile('target.json', function (err, data) {
+      if (err)
+          console.log(err);
+      else
+          target1 = data.toString()
+  });
+  // We don't want this to run on unrelated packets
+  if (!['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) return;
+  // Grab the channel to check the message from
+  const channel = bot.channels.get(packet.d.channel_id);
+  // There's no need to emit if the message is cached, because the event will fire anyway for that
+  if (channel.messages.has(packet.d.message_id)) return;
+  // Since we have confirmed the message is not cached, let's fetch it
+  channel.fetchMessage(packet.d.message_id).then(message => {
+      // Emojis can have identifiers of name:id format, so we have to account for that case as well
+      const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
+      // This gives us the reaction we need to emit the event properly, in top of the message object
+      const reaction = message.reactions.get(emoji);
+      // Adds the currently reacting user to the reaction's users collection.
+      if (reaction) reaction.users.set(packet.d.user_id, bot.users.get(packet.d.user_id));
+      // Check which type of event it is before emitting
+      if (packet.t === 'MESSAGE_REACTION_ADD') {
+          bot.emit('messageReactionAdd', reaction, bot.users.get(packet.d.user_id));
+      }
+      if (packet.t === 'MESSAGE_REACTION_REMOVE') {
+          bot.emit('messageReactionRemove', reaction, bot.users.get(packet.d.user_id));
+      }
+  });
+});
+bot.on('messageReactionAdd', (reaction, user) => {
+    if(reaction.message.id == target || target1){
+      if (user.bot) return;
+      reaction.remove(user)
+      ver.run(bot, reaction.message, user)
+    }else{
+      
+    }
+})
+bot.on('messageReactionRemove', (reaction, user) => {
+})
 
 bot.login(token)
---
+++ verfify/mc.js
@@ -0,0 +1,131 @@
+//const HypixelAPI = require('hypixel-api');
+const Discord = require('discord.js');
+const token = process.env.hypixel;
+const {
+    Hypixel,
+    Mojang
+} = require('hypixel-node');
+const hypixel = new Hypixel(token);
+const mojang = new Mojang();
+const rank = require('../calculater/rank.js')
+let response
+let response2
+var tar
+var name
+var mcname
+var boo = false
+exports.run = async (client, message, user) => {
+    var mee = await message.channel.send(`${user}請輸入你的暱稱`)
+    response = await message.channel.awaitMessages(m => m.author.id === user.id, {
+        maxMatches: 1,
+        time: 10000,
+        errors: ['time']
+    }).then(async function(arg) {
+        mee.delete()
+        arg.first().delete()
+        name = arg.first().content
+        tar = await message.channel.send(`${user}請輸入你的mc id(沒有請輸入"no mc id"注意空格大小寫)`)
+        response2 = await message.channel.awaitMessages(m => m.author.id === user.id, {
+            maxMatches: 1,
+            time: 10000,
+            errors: ['time']
+        }).then(async function(arg1) {
+            tar.delete()
+            arg1.first().delete()
+            mcname = arg1.first().content
+            if (arg1.first().content == 'no mc id') {
+                message.guild.fetchMember(user)
+                    .then(target => target.setNickname(name + '<' + '>'))
+            } else {
+                var t1 = await hypixel.getPlayer(mcname)
+                console.log(t1)
+                if (!t1) {
+                    while (!boo) {
+                        tar1 = await message.channel.send(`${user}請輸入有效的mc id`)
+                        response3 = await message.channel.awaitMessages(m => m.author.id === user.id, {
+                            maxMatches: 1,
+                            time: 10000,
+                            errors: ['time']
+                        }).then(async function(w) {
+                            tar1.delete()
+                            w.first().delete()
+                            t1 = await hypixel.getPlayer(w.first().content)
+                            if (t1) {
+                                boo = true
+                                mcname = w.first().content
+                            }
+                        }).catch(function(err) {
+                          console.error(err)
+                          tar1.delete()
+                          return message.channel.send('超時').then(m => m.delete(5000));
+                          boo = true
+                      })
+                    }
+                }
+                var user1 = await message.guild.fetchMember(user)
+                    
+                    user1.setNickname(name + '<' + t1.displayname + '>')
+                    let playerGuild = (await hypixel.getGuildByPlayer(t1.uuid));
+                    let rank1 = rank.generateFormattedRank(t1.rank || t1.packageRank || t1.newPackageRank)
+                    let rankrole = message.guild.roles.find('name', rank1);
+                    if (!rankrole) {
+                      try{
+                      rankrole = await message.guild.createRole({
+                        name: rank1,
+                        color: "#000000",
+                        permissions:[]
+                      })
+                    }catch(e){
+                      console.log(e.stack);
+                    }
+                  }
+                    await(user1.addRole(rankrole.id));
+                    if(playerGuild) {
+                    let playerGuildID = playerGuild._id
+                    let playerGuildName = playerGuild.name
+                    if (playerGuildID === '5bfe87a06d7ba9963a35042a') {
+                      let gRole = message.guild.roles.find('name', '普通會員<Member>');
+                      if (!gRole) {
+                        try{
+                        gRole = await message.guild.createRole({
+                          name: '普通會員<Member>',
+                          color: "#000000",
+                          permissions:[]
+                        })
+                      }catch(e){
+                        console.log(e.stack);
+                      }
+                    }
+                      await(user1.addRole(gRole.id));
+                      return;
+                    }else {
+                      let role = message.guild.roles.find('name', playerGuildName);
+                      if (!role) {
+                        try{
+                        role = await message.guild.createRole({
+                          name: playerGuildName,
+                          color: "#000000",
+                          permissions:[]
+                        })
+                      }catch(e){
+                        console.log(e.stack);
+                      }
+                    }
+                  }
+                      let friend = message.guild.roles.find('name', '🌈好捧油🌈<Friend>');
+                      await(user1.addRole(role.id));
+                      await(user1.addRole(friend.id));
+                   }
+            }
+
+        }).catch(function(err) {
+          console.error(err)
+          tar.delete()
+          return message.channel.send('超時或超過長度').then(m => m.delete(5000));
+      })
+    }).catch(function(err) {
+        console.error(err)
+        mee.delete()
+        return message.channel.send('超時或超過長度').then(m => m.delete(5000));
+    })
+}
